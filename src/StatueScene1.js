267b2e4f65a180ce3b50b077aa0c8f71d930ac7b
FrancescoGigli <gigli96francesco@gmail.com>
2021-07-15 21:56:51 +0200

    New (repo): add a room scene

--- src/StatueScene1.js
+++ src/StatueScene1.js
@@ -1,4 +1,4 @@
-import testfile from "../models/statueGLTF/scene.gltf";
+import testfile from "../models/newScene/scene.gltf";
 import sky from "../models/backgrounds/sky_texture.jpg";
 import * as THREE from 'three';
 import {
@@ -9,7 +9,8 @@ import {
     PerspectiveCamera,
     PointLight,
     SphereBufferGeometry,
-    TextureLoader
+    TextureLoader,
+    Vector3
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { DEFAULT_LAYER, loader, OCCLUSION_LAYER, renderer, updateShaderLightPosition } from "./index";
@@ -20,9 +21,10 @@ export class StatueScene1 extends BaseScene {
     constructor() {
         super();
         
-        this.camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 0.1, 10000)
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 30, 10000)
         this.controls = new OrbitControls(this.camera, renderer.domElement);
-
+    
+        this.cameraInitialPosition = new Vector3(-14, 0, -20)
         this.effectComposer = this.composeEffects()
         this.occlusionComposer = this.effectComposer[0]
         this.sceneComposer = this.effectComposer[1]
@@ -48,6 +50,8 @@ export class StatueScene1 extends BaseScene {
 
     update(){
         updateShaderLightPosition(this.lightSphere, this.camera, this.shaderUniforms)
+      
+        console.log(this.camera.position);
     }
 
 
@@ -58,6 +62,7 @@ export class StatueScene1 extends BaseScene {
                     let material = new THREE.MeshBasicMaterial({ color: "#000000" });
                     let occlusionObject = new THREE.Mesh(obj.geometry, material)
                     //obj.add(axesHelper);
+                    occlusionObject.rotation.x =100;
                     occlusionObject.add(new THREE.AxesHelper(100));
                     occlusionObject.layers.set(OCCLUSION_LAYER)
                     if (obj.parent != null) {
@@ -66,26 +71,49 @@ export class StatueScene1 extends BaseScene {
 
                 }
             })
-
+            this.gui.addFolder("Scene Position");
             this.scene.add(gltf.scene);
-            gltf.scene.position.x = 3;
-            gltf.scene.position.y = -0.5;
-            gltf.scene.position.z = 5;
+            
+            gltf.scene.position.x = -426;
+            gltf.scene.position.y = -78;
+            gltf.scene.position.z = 246;
+            
+/*
+            let posX = this.gui.add(gltf.scene.position, "x", -1000, 1000, 1);
+            let posY = this.gui.add(gltf.scene.position, "y", -1000, 1000, 1);
+            let posZ = this.gui.add(gltf.scene.position, "z", -1000, 1000, 1);
+            posX.onChange(x => {
+                gltf.scene.position.x = x;  
+            })
+            posY.onChange(y => {
+                gltf.scene.position.y = y;  
+            })
+            posZ.onChange(z => {
+                gltf.scene.position.z = z;  
+            })
+            */
+            
+            gltf.scene.rotateOnAxis.x = 100;
+            gltf.scene.rotateY = 100;
+            gltf.scene.rotateZ = 100
 
-
-
+            
         }, function (error) {
             //  console.error( error );
         });
+
+        
         
         this.scene.add(new AxesHelper(10))
-        
+        this.camera.position.x = -115;
+        this.camera.position.y = -1.2;
+        this.camera.position.z = -68;
 
-        this.camera.position.z = 200;
         this.controls.update();
         this.buildLight(this.scene);
-        this.buildBackGround()
+       // this.buildBackGround()
 
+     
     }
 
     buildLight(){
@@ -100,10 +128,11 @@ export class StatueScene1 extends BaseScene {
         
 
 
-        let geometry = new THREE.SphereBufferGeometry(0.8, 32, 32);
+        let geometry = new THREE.SphereBufferGeometry(80, 320, 320);
         let material = new THREE.MeshBasicMaterial({ color: 0xffffff });
         this.lightSphere = new THREE.Mesh(geometry, material);
         this.lightSphere.layers.set(OCCLUSION_LAYER)
+        this.lightSphere.position.z = -700
 
         this.scene.add(this.lightSphere);
 
@@ -128,9 +157,10 @@ export class StatueScene1 extends BaseScene {
 
     buildGUI() {
         this.gui.addFolder("Light Position");
-        let xController = this.gui.add(this.lightSphere.position, "x", -10, 10, 0.01);
-        let yController = this.gui.add(this.lightSphere.position, "y", -10, 10, 0.01);
-        let zController = this.gui.add(this.lightSphere.position, "z", -20, 20, 0.01);
+        let xController = this.gui.add(this.lightSphere.position, "x", -1000, 1000, 1);
+        let yController = this.gui.add(this.lightSphere.position, "y", -1000, 1000, 1);
+        let zController = this.gui.add(this.lightSphere.position, "z", -2000, 2000, 1);
+
 
 
         this.controls.addEventListener("change", () => updateShaderLightPosition(this.lightSphere, this.camera, this.shaderUniforms))
